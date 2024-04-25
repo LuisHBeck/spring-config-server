621d1e4682ab304be6deb4157a7aa51ec5893267
LuisHBeck <beck.henrique.luis@gmail.com>
2024-04-25 14:01:08 -0300

    feat: add @RefreshScope (don't have to restart the server)

--- order-ms/src/main/java/com/beck/orderms/controller/OrderController.java
+++ order-ms/src/main/java/com/beck/orderms/controller/OrderController.java
@@ -1,12 +1,14 @@
 package com.beck.orderms.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
+@RefreshScope
 public class OrderController {
 
     @Value("${welcome.message}")
