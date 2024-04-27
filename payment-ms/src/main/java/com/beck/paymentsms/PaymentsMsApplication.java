3df74eb005707f353adf081304ef2b8401257bba
LuisHBeck <beck.henrique.luis@gmail.com>
2024-04-27 19:51:40 -0300

    feat: add payment service

---
+++ payment-ms/src/main/java/com/beck/paymentsms/PaymentsMsApplication.java
@@ -0,0 +1,13 @@
+package com.beck.paymentsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsMsApplication.class, args);
+	}
+
+}
---
+++ payment-ms/src/test/java/com/beck/paymentsms/PaymentsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beck.paymentsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
